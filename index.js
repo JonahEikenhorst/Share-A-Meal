c83510b2648c0074ab16f3d62d02708ec065058d
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2022-04-22 12:03:47 +0200

    Added email constraint

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.get('/', (req, res) => {
   });
 });
 
+// Meal POST, GET
+//------------------------------------------
 app.post('/api/meal', (req, res) => {
   let meal = req.body;
   console.log(meal);
@@ -48,7 +50,7 @@ app.get('/api/meal', (req, res) => {
 })
 
 app.get('/api/meal/:mealId'), (req, res) => {
-  const mealId = req.params.movieId
+  const mealId = req.params.mealId
   let meal = database.filter((item) => item.id == mealId);
   if (meal.length > 0) {
     console.log(meal)
@@ -65,6 +67,117 @@ app.get('/api/meal/:mealId'), (req, res) => {
   }
 };
 
+//--------------------------------------------------------------
+//User POST, GET
+app.post('/api/user', (req, res) => {
+  let email = req.body.emailAddress;
+    if (database.filter((item) => item.emailAddress == email).length > 0) {
+        res.status(400).json({
+            Status: 400,
+            Message: `An user with this Email adress already exists!`
+        })
+    }
+    else {
+  let user = req.body;
+  console.log(user);
+  id++
+  user = {
+    id,
+    ...user,
+  };
+
+  database.push(user);
+  console.log(database);
+  res.status(201).json({
+    status: 201,
+    result: database,
+  });
+}});
+
+
+app.get('/api/user', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: database,
+
+  })
+})
+
+app.get('/api/user/profile', (req, res) => {
+  res.status(401).json({
+    status: 401,
+    result: "End-Point is not yet realised",
+
+  })
+})
+
+app.get('/api/user/:userId', (req, res) => {
+  const userId = req.params.userId
+  let user = database.filter((item) => item.id == userId);
+  if (user.length > 0) {
+    console.log(user)
+    res.status(200).json({
+      status: 200,
+      result: user,
+    })
+  }
+  else {
+    res.status(404).json({
+      status: 404,
+      result: `User with ID ${userId} not found`,
+    });
+  }
+});
+
+app.delete('/api/user/:userId', (req, res) => {
+  const userId = req.params.userId
+  let user = database.filter((item) => item.id == userId);
+  if (user.length > 0) {
+    console.log(user)
+    database.splice(user.indexOf()-1,1);
+    res.status(200).json({
+      status: 200,
+      result: `User with ID ${userId} has been deleted`,
+    })
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: `User with ID ${userId} not found`,
+    });
+  }
+});
+
+app.put('/api/user/:userId', (req, res) => {
+  const userId = req.params.userId
+  let user = database.filter((item) => item.id == userId);
+  if (user.length > 0) {
+
+    console.log(user)
+    database.splice(user.indexOf()-1,1);
+
+    let newUser = req.body;
+    console.log(newUser);
+    newUser = {
+      userId,
+      ...newUser,
+   };
+    database.push(newUser);
+    console.log(database);
+
+    res.status(200).json({
+      status: 200,
+      result: `User with ID ${userId} has been updated,`
+    })
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: `User with ID ${userId} not found,`
+    });
+  }
+});
+
+// -----------------------------------------------------------
+//Cant find location
 app.all("*", (req, res) => {
   res.status(401).json({
     status: 401,
@@ -72,5 +185,5 @@ app.all("*", (req, res) => {
   })
 })
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Meal app listening on port ${port}`)
 });
