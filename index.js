8ec3fb9ff9f583810567c86e1c0e41d7a03daae6
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2022-04-23 21:36:55 +0200

    Edit user fixed

--- index.js
+++ index.js
@@ -130,48 +130,70 @@ app.get('/api/user/:userId', (req, res) => {
 });
 
 app.delete('/api/user/:userId', (req, res) => {
-  const userId = req.params.userId
+  const userId = Number(req.params.userId)
+  //check if id is in database
   let user = database.filter((item) => item.id == userId);
   if (user.length > 0) {
-    console.log(user)
-    database.splice(user.indexOf()-1,1);
+
+    //check the index of userId
+    var index = database.map(x => {
+      return x.id;
+    }).indexOf(userId);
+
+    //Remove the id from array
+    console.log("INDEX OF "+index)
+    database.splice(index, 1);
+    console.log(database);
+
     res.status(200).json({
       status: 200,
-      result: `User with ID ${userId} has been deleted`,
+      result: `User with ID ${userId} has been deleted.`,
     })
   } else {
     res.status(404).json({
       status: 404,
-      result: `User with ID ${userId} not found`,
+      result: `User with ID ${userId} not found.`,
     });
   }
 });
 
 app.put('/api/user/:userId', (req, res) => {
-  const userId = req.params.userId
-  let user = database.filter((item) => item.id == userId);
+  const id = Number(req.params.userId)
+  let email = req.body.Email;
+  let user = database.filter((item) => item.id == id);
+
   if (user.length > 0) {
+    if (database.filter((item) => item.Email == email).length > 0) {
+        res.status(400).json({
+            Status: 400,
+            Message: `This email is already taken.`
+        })
+    } else {
+      var index = database.map(x => {
+        return x.id;
+      }).indexOf(id);
 
-    console.log(user)
-    database.splice(user.indexOf()-1,1);
+      console.log("INDEX OF "+index)
+      database.splice(index, 1);
+      console.log(database);
 
-    let newUser = req.body;
-    console.log(newUser);
-    newUser = {
-      userId,
-      ...newUser,
-   };
-    database.push(newUser);
-    console.log(database);
-
-    res.status(200).json({
-      status: 200,
-      result: `User with ID ${userId} has been updated,`
-    })
+      let newUser = req.body;
+      console.log(newUser);
+      newUser = {
+        id,
+        ...newUser,
+     };
+      database.push(newUser);
+      console.log(database);
+      res.status(200).json({
+        status: 200,
+        result: User `with ID ${id} has been updated.`,
+      })
+    }
   } else {
     res.status(404).json({
       status: 404,
-      result: `User with ID ${userId} not found,`
+      result: `User with ID ${id} not found.`,
     });
   }
 });
