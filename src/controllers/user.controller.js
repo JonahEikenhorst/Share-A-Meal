ce6d307170613395c247e4e2e30ab74a781aa55f
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2022-05-19 14:53:22 +0200

    added JSONwebtoken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const { AssertionError } = require('assert');
 const assert = require('assert');
-let database = [];
-let id = 0;
+const dbconnection = require('../../database/dbconnection');
+
 let controller = {
     validateUser: (req, res, next) => {
         let user = req.body;
@@ -50,11 +50,33 @@ let controller = {
             });
         }
     },
-    getUsers: (req, res) => {
-        res.status(200).json({
-            status: 200,
-            result: database
-        });
+
+    getUsers: (req, res, next) => {
+        dbconnection.getConnection(function (err, connection) {
+            if (err) throw err // not connected!
+
+            // Use the connection
+            connection.query(
+                'SELECT * FROM user;',
+                function (error, results, fields) {
+                    // When done with the connection, release it.
+                    connection.release()
+
+                    // Handle error after the release.
+                    if (error) throw error
+
+                    // Don't use the connection here, it has been returned to the pool.
+                    console.log('#results = ', results.length)
+                    res.status(200).json({
+                        status: 200,
+                        results: results,
+                    })
+                    // pool.end((err) => {
+                    //     console.log('pool was closed.')
+                    // })
+                }
+            )
+        })
     },
 
     getUserById: (req, res, next) => {
